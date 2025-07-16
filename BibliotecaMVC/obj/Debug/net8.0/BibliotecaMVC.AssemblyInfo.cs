2281513f2ff36ad4a1eb9e7c5bec9bcdfb56e28c
Ligia <s.morais.ligia@gmail.com>
2025-07-16 10:37:10 -0300

    Mais correções de Views

--- BibliotecaMVC/obj/Debug/net8.0/BibliotecaMVC.AssemblyInfo.cs
+++ BibliotecaMVC/obj/Debug/net8.0/BibliotecaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea112404471086ea6fb8100c837b884967e75a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e85e10e5586c9803b6f9c9df3688598415e323")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
